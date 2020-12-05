1810196d53afc497e2069492c1d601b497598094
Ram Bombeo <micrabom@gmail.com>
2020-12-05 22:26:02 +0800

    Add project files.

---
+++ EntryPoint/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class EntryPoint
+{
+    static IWebDriver driver = new ChromeDriver();
+    
+
+
+    static void Main()
+    {
+        string url = "google.com";
+
+
+        /* ---------  ---------  Navigate to Blazor7 --------- */
+        driver.Navigate().GoToUrl("url");
+        /* ---------  --------- Delay 7sec --------- */
+        Thread.Sleep(5000);
+
+        /* ---------  --------- Find and click one of the Jobs --------- */
+        /* ---------  --------- Delay 5ec*/
+        driver.FindElement(By.Id("b7-jobNumber-1")).Click();
+        Thread.Sleep(30000);
+
+        driver.Quit();
+
+    }
+}
